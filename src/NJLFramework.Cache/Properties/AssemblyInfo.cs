8deec8ab03d994c848503fdd5ece11e0bb4c654b
wdf <2850968286@qq.com>
2016-08-30 20:38:09 +0800

    测试

--- src/NJLFramework.Cache/Properties/AssemblyInfo.cs
+++ src/NJLFramework.Cache/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(false)] 
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("247f1357-70a2-4f31-ab9a-a9f8ffb8e8fc")]
